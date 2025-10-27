cffb19c0bd1f58b1f128d28dfaa8c26a85f2c087
jongchul94 <kjc94kr@hanmail.net>
2025-10-27 18:26:19 +0900

    commit

---
+++ programmers/programmers 0-36.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> list = new ArrayList<Integer>();
+        switch (n){
+            case 1:
+                for(int i=0;i<=slicer[1];i++){
+                    list.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for(int i=slicer[0];i<num_list.length;i++){
+                    list.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for(int i=slicer[0];i<=slicer[1];i++){
+                    list.add(num_list[i]);
+                }
+                break;
+            case 4:
+                for(int i=slicer[0];i<=slicer[1];i+=slicer[2]){
+                    list.add(num_list[i]);
+                }
+                break;
+        }
+        int[] answer = new int[list.size()];
+        for(int j=0;j<list.size();j++){
+            answer[j] = list.get(j);
+        }
+        return answer;
+    }
+}
