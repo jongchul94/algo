d8d176007324f7e8d0eff82114a7b6cd0e8a5de2
jongchul94 <kjc94kr@hanmail.net>
2025-10-31 11:18:10 +0900

    commit

---
+++ programmers/programmers 0-42.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] arr1 = Arrays.copyOfRange(num_list,n,num_list.length);
+        int[] arr2 = Arrays.copyOfRange(num_list,0,n);
+        int[] answer = Arrays.copyOfRange(arr1,0,arr1.length+arr2.length);
+        System.arraycopy(arr2,0,answer,arr1.length,arr2.length);
+        return answer;
+    }
+}
