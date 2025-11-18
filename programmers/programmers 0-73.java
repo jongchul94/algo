3e29a0af0012ec1989ed13eb79a7eb37ae10ef9e
jongchul94 <kjc94kr@hanmail.net>
2025-11-18 14:21:21 +0900

    commit

---
+++ programmers/programmers 0-73.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        int i = 0;
+        while(i<arr.length){
+            if(list.size() == 0){
+                list.add(arr[i]);
+                i++;
+            }else if(list.get(list.size()-1) == arr[i]){
+                list.remove(list.size()-1);
+                i++;
+            }else if(list.get(list.size()-1) != arr[i]){
+                list.add(arr[i]);
+                i++;
+            }
+        }
+        if(list.size() == 0){
+            list.add(-1);
+        }
+        int[] answer = new int[list.size()];
+        for(int j=0;j<list.size();j++){
+            answer[j] = list.get(j);
+        }
+        return answer;
+    }
+}
