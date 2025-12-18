75fd0c0915a77f62582e91a6b6c3e6351b454c76
jongchul94 <kjc94kr@hanmail.net>
2025-12-18 17:06:22 +0900

    example

--- programmers/programmers 0-103.java
+++ programmers/programmers 0-103.java
@@ -22,3 +22,17 @@ class Solution {
         return answer;
     }
 }
+
+
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
