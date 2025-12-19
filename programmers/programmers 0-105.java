6acdc0ef0fcb6cdec90fa5277c02c5a09e76b0e8
jongchul94 <kjc94kr@hanmail.net>
2025-12-19 17:47:28 +0900

    commit

---
+++ programmers/programmers 0-105.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        if(num%2==0){
+            for(int i=0;i<num;i++){
+                answer[i] = total/num - (num/2-1-i);
+            }
+        }else if(num%2==1){
+            for(int i=0;i<num;i++){
+                answer[i] = total/num - (num/2-i);
+            }
+        }
+        return answer;
+    }
+}
